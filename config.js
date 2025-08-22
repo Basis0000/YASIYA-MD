a21974cc435fc684c36feeaa5677a1257becd033
Basis0000 <144522211+Basis0000@users.noreply.github.com>
2025-08-22 08:19:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?JzekU6'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?as3nZB'
 };
